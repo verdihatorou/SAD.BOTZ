524e6cd86c88ffb418dbf423cf958349e48bfdcc
JakaV5 <108243145+JakaV5@users.noreply.github.com>
2022-07-03 10:23:33 +0700

    Create audio-assalamualaikum.js

---
+++ plugins/audio-assalamualaikum.js
@@ -0,0 +1,17 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+let vn = './media/Assalamualaikum.mp3'
+conn.sendFile(m.chat, vn, 'Assalamualaikum.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.help = ['assalamualaikum']
+handler.tags = ['sound']
+handler.customPrefix = /(assalamualaikum|Assalamualaikum|assalamu'alaikum|Assalamu'alaikum)/i
+handler.command = new RegExp
+handler.fail = null
+handler.exp = 100
+export default handler
