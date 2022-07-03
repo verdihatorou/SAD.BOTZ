a4812a60ed01b2a1bbb15e207546b4c1eba13bf4
JakaV5 <108243145+JakaV5@users.noreply.github.com>
2022-07-03 10:12:43 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 INTRODUCTION 』*', `Hai kak, Aku adalah bot WhatsApp, Aku diberi nama CntkaBotz, Yang dibuat Oleh manusia gabut yang bernama Anggara,dan dia masih pemula, jadi kalau ada yang error mohon dimaklumi ya:v\nSilakan klik tombol menu ya kak, dan mohon jangan di spam\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
-[`Menu`, '.m'],
+[`Menu`, '.craft'],
 [`\nAku gila dan aku bangga:v`, 'Bilek']
 ], m, {asLocation: true})
 }
