eb6f2e108cec1541bd7dc18342d16ae3ef0cace0
JakaV5 <108243145+JakaV5@users.noreply.github.com>
2022-07-03 10:15:35 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/FahriAdison/Sad-Multi-Device*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
+let str = `*GK ADA SC SC AN CODING SENDIRI SONO*\n© _Anggara_`
+conn.sendButtonDoc(m.chat, str, wm, 'Owner','.owner', ftextt, m)
 }
 handler.command = handler.help = ['sc']
 handler.tags = ['info']
