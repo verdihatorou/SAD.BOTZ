12584dad93a8c6c2d3c00f4ac1851f01502186d1
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-07 13:43:02 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,1 +1,17 @@
-function _0x2d81(_0x472c45,_0x4f98b9){const _0x70d0ff=_0x70d0();return _0x2d81=function(_0x2d815d,_0x632d09){_0x2d815d=_0x2d815d-0x79;let _0x251e0e=_0x70d0ff[_0x2d815d];return _0x251e0e;},_0x2d81(_0x472c45,_0x4f98b9);}const _0x1db064=_0x2d81;(function(_0x1d7b35,_0x38aaba){const _0x22bec8=_0x2d81,_0x4dadea=_0x1d7b35();while(!![]){try{const _0x10e4b2=parseInt(_0x22bec8(0x83))/0x1*(parseInt(_0x22bec8(0x7e))/0x2)+-parseInt(_0x22bec8(0x82))/0x3+parseInt(_0x22bec8(0x8a))/0x4*(-parseInt(_0x22bec8(0x89))/0x5)+-parseInt(_0x22bec8(0x7c))/0x6*(-parseInt(_0x22bec8(0x86))/0x7)+-parseInt(_0x22bec8(0x8d))/0x8+-parseInt(_0x22bec8(0x80))/0x9*(parseInt(_0x22bec8(0x88))/0xa)+-parseInt(_0x22bec8(0x7a))/0xb*(-parseInt(_0x22bec8(0x91))/0xc);if(_0x10e4b2===_0x38aaba)break;else _0x4dadea['push'](_0x4dadea['shift']());}catch(_0x48b040){_0x4dadea['push'](_0x4dadea['shift']());}}}(_0x70d0,0x56963));let util=require(_0x1db064(0x7f)),path=require(_0x1db064(0x8f)),fetch=require(_0x1db064(0x8b)),handler=async(_0x432aba,{conn:_0x1f39f7})=>{const _0x479167=_0x1db064;let _0x209cd6='./haori.m4a';_0x1f39f7[_0x479167(0x90)](_0x432aba[_0x479167(0x7b)],_0x209cd6,_0x479167(0x84),null,_0x432aba,!![],{'type':_0x479167(0x85),'ptt':!![],'contextInfo':{'forwardingScore':0x3e7,'isForwarded':!![],'externalAdReply':{'title':_0x479167(0x8c),'body':_0x479167(0x8e),'description':'','mediaType':0x2,'thumbnail':await(await fetch(_0x479167(0x87)))['buffer'](),'mediaUrl':_0x479167(0x92)}}});};function _0x70d0(){const _0x4dcc5a=['16xHKNGK','araara.m4a','audioMessage','177709ABgmXr','https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg','811090fNxXqF','43205XKswkX','36JwTzLy','node-fetch','Recorde\x20Sendiri\x20Kapan\x20Bisa\x20Nya\x20Kalo\x20Pakai\x20Yang\x20Orang','3132104swbVKH','Belajar\x20Recorde\x20Sendiri\x20Hargain\x20Yang\x20Buat\x20Nya','path','sendFile','16503132LGNFqE','https://youtu.be/35w7z9QFLwY','help','11gayAln','chat','18WiXwql','exports','956OqNcUk','util','9QbvyCE','command','1662060ArJEfD'];_0x70d0=function(){return _0x4dcc5a;};return _0x70d0();}handler[_0x1db064(0x79)]=['sourcecode'],handler['tags']=['info'],handler[_0x1db064(0x81)]=/^(sourcecode|sc|scbot|script|github)$/i,module[_0x1db064(0x7d)]=handler;
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*Nyarii Sc Banh? Chat #owner :v*\n© _Fachri_`
+conn.sendButtonDoc(m.chat, str, wm, 'Owner','.owner', ftextt, m)
+}
+} })
+conn.sendFile(m.chat, vn, './haori.m4a', null, fkon, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+handler.command = handler.help = ['sc']
+handler.tags = ['info']
+
+export default handler
