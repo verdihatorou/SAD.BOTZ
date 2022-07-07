005a76ded19c229b270dffd673fd769c945d6c9c
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-07 09:38:11 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*GK ADA SC SC AN CODING SENDIRI SONO*\n© _Anggara_`
+let str = `*Mau Sc Banh? Chat Owner:v*\n© _Fachri_`
 conn.sendButtonDoc(m.chat, str, wm, 'Owner','.owner', ftextt, m)
 }
 handler.command = handler.help = ['sc']
