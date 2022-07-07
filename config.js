80ddc0ae2901b939640e3a39da2ea13142ca0dd5
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-07 11:24:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -67,8 +67,8 @@ global.flaaa = [
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
  ]
-global.sgc = 'https://chat.whatsapp.com/GyKM8sqX8A2DQD4p3ZJUWz'
-global.sig = 'https://instagram.com/sfdesign.id'
+global.sgc = 'https://chat.whatsapp.com/GimZmhA2XTQFfPXk4XMrv6'
+global.sig = 'https://instagram.com/sfdesign_id'
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
 global.urlnya = "https://www.instagram.com/sfdesign.id"
