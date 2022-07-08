902efc89aabe9dcaccdb58c50dfc7ff7a728474e
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 19:36:31 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,25 +1,27 @@
-let util = require('util')
-let path = require('path')
-let fetch = require('node-fetch')
+import util from 'util'
+import fetch from 'node-fetch'
+import path from 'path'
+
 let handler = async (m, { conn }) => {
-let vn = './bernyanyi.mp3'
-conn.sendFile(m.chat, vn, 'bernyanyi.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true, contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/sfdesign_id",
-    mediaType: "VIDEO",
-    description: "https://Instagram.com/sfdesign_id", 
-    title: 'Recode Sendiri Yaw kak ><',
-    body: wm,
-    thumbnail: thumb,
-    sourceUrl: sgc
-} 
+let vn = './media/Gaboleh-gitu.mp3'
+conn.sendFile(m.chat, vn, 'Gaboleh-gitu.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true , contextInfo: { showAdAttribution: true,
+         externalAdReply: { 
+             title: 'Recorde Sendiri Kapan Bisa Nya Kalo Pakai Yang Orang',  
+             body: 'Belajar Recorde Sendiri Hargain Yang Buat Nya', 
+             description: '', 
+             mediaType: 2, 
+           thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
+          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
          } 
       } 
    })
 }
 handler.help = ['sourcecode']
-handler.tags = ['main']
+handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
-
-module.exports = handler
+handler.command = new RegExp
+handler.fail = null
+handler.exp = 100
+export default handler
