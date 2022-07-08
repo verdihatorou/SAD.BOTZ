804de89fdef44176e46a3154a836dcec7db9fd3f
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 19:05:27 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-let vn = './haori.m4a'
+let vn = './haori.mp3'
 conn.sendFile(m.chat, vn, 'araara.m4a', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true, contextInfo: { forwardingScore: 999, isForwarded: true,
@@ -13,31 +13,6 @@ ptt: true, contextInfo: { forwardingScore: 999, isForwarded: true,
              mediaType: 2, 
            thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
           mediaUrl: `https://instagram.com/sfdesign_id` 
-         } 
-      } 
-   })
-}
-handler.command = handler.help = ['sc']
-handler.tags = ['info']
-
-export default handler
-
-
-let util = require('util')
-let path = require('path')
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
-let vn = './haori.m4a'
-conn.sendFile(m.chat, vn, 'araara.m4a', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true, contextInfo: { forwardingScore: 999, isForwarded: true,
-         externalAdReply: { 
-             title: 'Recorde Sendiri Kapan Bisa Nya Kalo Pakai Yang Orang',  
-             body: 'Belajar Recorde Sendiri Hargain Yang Buat Nya', 
-             description: '', 
-             mediaType: 2, 
-           thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
-          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
          } 
       } 
    })
