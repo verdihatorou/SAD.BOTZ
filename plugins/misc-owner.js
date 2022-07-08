cc95a0f7518a5279fd7cf7a4a510c891e9b73d98
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 20:39:36 +0700

    Update misc-owner.js

--- plugins/misc-owner.js
+++ plugins/misc-owner.js
@@ -1,30 +1,7 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-let handler = async (m, {conn}) => {
-const vcard = `BEGIN:VCARD
-VERSION:3.0
-N:Sy;Bot;;;
-FN: Fachri 
-item.ORG: Tak ingin terkenal hanya ingin di kenal
-item1.TEL;waid=6285713041886:6285713041886@s.whatsapp.net
-item1.X-ABLabel:Sibuk 
-item2.EMAIL;type=INTERNET:saefulfachri18@gmail.com
-item2.X-ABLabel:Email
-item3.ADR:;;🇮🇩 Indonesia;;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:Region
-item4.URL:https://fachriweb.vercel.app
-item4.X-ABLabel:Website
-END:VCARD`
-const sentMsg  = await conn.sendMessage(
-    m.chat,
-    { 
-        contacts: { 
-            displayName: 'OWNER GW', 
-            contacts: [{ vcard }] 
-        }
-    }
-)
-await conn.sendButton(m.chat, "itu Nomor Orang Ganteng", 'jgn sungkan chat ya kak', [['Group Official', _p + 'gcbott'], ['Owner', _p + 'owner']], sentMsg)}
+function handler(m) {
+  let data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak)
+}
 handler.alias = ['owner']
 handler.command = /^(owner)$/i
 
