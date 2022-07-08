41ce8fd19f6d861daf507f4e5c4e7c35c5536370
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 19:04:27 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -12,7 +12,7 @@ ptt: true, contextInfo: { forwardingScore: 999, isForwarded: true,
              description: '', 
              mediaType: 2, 
            thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
-          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
+          mediaUrl: `https://instagram.com/sfdesign_id` 
          } 
       } 
    })
@@ -23,4 +23,27 @@ handler.tags = ['info']
 export default handler
 
 
+let util = require('util')
+let path = require('path')
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+let vn = './haori.m4a'
+conn.sendFile(m.chat, vn, 'araara.m4a', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true, contextInfo: { forwardingScore: 999, isForwarded: true,
+         externalAdReply: { 
+             title: 'Recorde Sendiri Kapan Bisa Nya Kalo Pakai Yang Orang',  
+             body: 'Belajar Recorde Sendiri Hargain Yang Buat Nya', 
+             description: '', 
+             mediaType: 2, 
+           thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
+          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
+         } 
+      } 
+   })
+}
+handler.help = ['sourcecode']
+handler.tags = ['main']
+handler.command = /^(sourcecode|sc|scbot|script|github)$/i
 
+module.exports = handler
