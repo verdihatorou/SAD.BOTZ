8e8d8aa5e34956518d0a0cf528279d1974b17af0
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 21:04:25 +0700

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args }) => {
 	if (!res) throw 'Can\'t download the post'
 	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
 	await m.reply('_In progress, please wait..._')
-	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: m })
+	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: fkontak })
 }
 handler.help = ['facebook']
 handler.tags = ['downloader']
