0c51340f93396ececa2bad1439f78a65c58697c9
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-08 20:55:39 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-let vn = './media/Gaboleh-gitu.mp3'
-conn.sendFile(m.chat, vn, 'Gaboleh-gitu.mp3', null, m, true, {
+let vn = './media/AUD-20220708-WA0325.mp3'
+conn.sendFile(m.chat, vn, 'Gaboleh-gitu.mp3', null, fkontak, true, {
 type: 'audioMessage', 
 ptt: true , contextInfo: { showAdAttribution: true,
          externalAdReply: { 
-             title: 'Recorde Sendiri Kapan Bisa Nya Kalo Pakai Yang Orang',  
-             body: 'Belajar Recorde Sendiri Hargain Yang Buat Nya', 
+             title: 'Recorde Sendiri Kapan Bisa Nya Kalo Pakai Sc Orang',  
+             body: 'Belajar Recorde Sendiri Hargain Yang Buat Nya ><', 
              description: '', 
              mediaType: 2, 
            thumbnail: await (await fetch('https://telegra.ph/file/a9a6907db67ca1a0c0877.jpg')).buffer(), 
