3691a69fe1853d448d2ea82029574872d912f8b0
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-09 09:08:03 +0700

    Update owner-getsesi.js

--- plugins/owner-getsesi.js
+++ plugins/owner-getsesi.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Sedang mengambil file sesi mu')
     let sesi = await fs.readFileSync('./session.data.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: fkontak })
 }
 handler.help = ['getsessi']
 handler.tags = ['owner']
