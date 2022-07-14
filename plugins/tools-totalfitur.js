7f7c67e3f2363f6cf25d06065eebb4f17a3e8cfe
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-14 19:44:24 +0700

    Update tools-totalfitur.js

--- plugins/tools-totalfitur.js
+++ plugins/tools-totalfitur.js
@@ -1,9 +1,21 @@
-let handler = async (m, { conn }) => {
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur saat ini: ${totalf}`, fkontak)
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: bottime,
+                        body: 'Total Cintaku Padamu',          previewType: 0,
+                        thumbnail: fs.readFileSync("./IMG-20220707-WA0022.jpg"),
+                        sourceUrl: sig
+                      }}
+})
 }
+
 
 handler.help = ['totalfitur']
 handler.tags = ['tools']
