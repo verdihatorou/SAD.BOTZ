a43d43dce409f8626decb2b010df2bd65a61ecb1
Perkenalkan nama saya <95448883+verdihatorou@users.noreply.github.com>
2022-07-16 16:16:57 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -5,13 +5,13 @@ let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├ Dana : 085713041886
-├ Indosat : 08159917553
+├ Dana : Gada
+├ Indosat : free
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 *Contact person Owner:*
-wa.me/6285713041886 (Fachri)`
-conn.sendHydrated(m.chat, str, wm, './media/IMG-20220706-WA0063.jpg', 'https://github.com/Ipulpachri', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+wa.me/6285706735450 (verdiuhuy)`
+conn.sendHydrated(m.chat, str, wm, './media/IMG-20220706-WA0063.jpg', 'https://github.com/verdihatorou', '𝙶𝙸𝚃𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 
